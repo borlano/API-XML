b465d53d0f4388672921a66c9eb6233941f2f243
alexey.a <borlano7@gmail.com>
2020-11-27 10:40:40 +0300

    cs fixer

--- src/Xml_api.php
+++ src/Xml_api.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace borlano\api_xml;
 
 use http\Exception;
@@ -6,7 +7,7 @@ use SimpleXMLElement;
 
 /**
  * Вспомогательный класс для работы с XML API программы RBS360
- * Документация API: https://developers.rbs360.ru/integraciya-api-xml/
+ * Документация API: https://developers.rbs360.ru/integraciya-api-xml/.
  */
 class Xml_api
 {
@@ -14,15 +15,19 @@ class Xml_api
      *    property string $url - хранение адреса сервера с XRM
      *    property string $login - хранение логина авторизации сервера с XRM
      *    property string $pass - хранение пароля авторизации сервера с XRM
-     *    property string $sess_id - хранение хэша сессии после авторизации сервера с XRM
+     *    property string $sess_id - хранение хэша сессии после авторизации сервера с XRM.
      */
     private $url;
+
     private $login;
+
     private $pass;
+
     private $sess_id;
 
     /**
-     * Создание подключения и получение sess_id
+     * Создание подключения и получение sess_id.
+     *
      * @param $url
      * @param $login
      * @param $pass
@@ -36,18 +41,31 @@ class Xml_api
     }
 
     /**
-     * Создание запроса на авторизацию и отправка его на сервер, в случае успеха установка свойства sess_id
+     * @return string
+     */
+    public function head()
+    {
+        return "<?xml version='1.0' encoding='utf8' ?><request>";
+    }
+
+    public function footer()
+    {
+        return '</request>';
+    }
+
+    /**
+     * Создание запроса на авторизацию и отправка его на сервер, в случае успеха установка свойства sess_id.
      */
     private function _auth()
     {
-        $request = "<?xml version='1.0' encoding='utf8' ?><request>
-			<action type='auth' uid='80085'>
-			<login>" . $this->login . "</login>
-			<password>" . $this->pass . "</password>
-			</action>
-			</request>";
+        $request = $this->head()
+            ."<action type='auth' uid='80085'>
+			<login>".$this->login.'</login>
+			<password>'.$this->pass.'</password>
+			</action>'
+            .$this->footer();
 
-        $response = $this->send($this->url, "POST", array("xml" => $request));
+        $response = $this->send($this->url, 'POST', ['xml' => $request]);
         if ($response_parse = self::_parse($response)) {
             $this->sess_id = $response_parse->action->sess_id;
         }
@@ -55,30 +73,32 @@ class Xml_api
 
     /**
      *    отправка xml запроса на сервер
+     *
      * @param $url
      * @param string $method
-     * @param array $args
-     * @param array $cookie
+     * @param array  $args
+     * @param array  $cookie
+     *
      * @return bool|string
      */
-    public function send($url, $method = "GET", $args = array(), $cookie = array())
+    public function send($url, $method = 'GET', $args = [], $cookie = [])
     {
         $ch = curl_init();
-        $query_string = "";
+        $query_string = '';
         foreach ($args as $key => $val) {
-            $query_string .= $key . "=" . $val . "&";
+            $query_string .= $key.'='.$val.'&';
         }
 
-        if ($method == "GET") {
-            $url .= "?" . $query_string;
+        if ($method == 'GET') {
+            $url .= '?'.$query_string;
         }
 
         curl_setopt($ch, CURLOPT_URL, $url);
 
-        $cookies = "";
+        $cookies = '';
 
         foreach ($cookie as $key => $val) {
-            $cookies .= "$key=" . $val . ";";
+            $cookies .= "$key=".$val.';';
         }
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -89,18 +109,21 @@ class Xml_api
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
-        if ($method == "POST") {
+        if ($method == 'POST') {
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
         }
 
         $resp = curl_exec($ch);
+
         return $resp;
     }
 
     /**
-     *    Парсинг xml c помощью библиотеки SimpleXMLElement
+     *    Парсинг xml c помощью библиотеки SimpleXMLElement.
+     *
      * @param $xml
+     *
      * @return SimpleXMLElement
      */
     private static function _parse($xml)
@@ -112,7 +135,7 @@ class Xml_api
     }
 
     /**
-     * Получение ID сессии
+     * Получение ID сессии.
      *
      * @return mixed
      */
@@ -122,149 +145,163 @@ class Xml_api
     }
 
     /**
-     *    создание запроса на получение данных
+     *    создание запроса на получение данных.
+     *
      * @param $structure
      * @param array $fields
      * @param array $filters
      * @param array $limits
      * @param array $orders
+     *
      * @return SimpleXMLElement
      */
-    public function select($structure, $fields = array(), $filters = array(), $limits = array(), $orders = array())
+    public function select($structure, $fields = [], $filters = [], $limits = [], $orders = [])
     {
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='list' uid='80085'>
-			<structure name='" . $structure . "'>" . "
-			" . $this->_prepareFields($fields) . "
-			" . $this->_prepareFilters($filters) . "
-			" . $this->_prepareOrders($orders) . "
-			" . $this->_prepareLimits($limits) . "     								
+			<structure name='".$structure."'>".'
+			'.$this->_prepareFields($fields).'
+			'.$this->_prepareFilters($filters).'
+			'.$this->_prepareOrders($orders).'
+			'.$this->_prepareLimits($limits).'     								
 			</structure>
 			</action>
-			</request>";
+			</request>';
 
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 
     /**
-     *    преобразование массива полей в xml
+     *    преобразование массива полей в xml.
+     *
      * @param $fields
+     *
      * @return string
      */
     private function _prepareFields($fields)
     {
-        $fields_xml = "<fields>";
+        $fields_xml = '<fields>';
         foreach ($fields as $field => $value) {
             if (is_numeric($field)) {
                 $field = $value;
-                $value = "";
+                $value = '';
             }
-            $fields_xml .= "
-				<" . $field . ">" . $value . "</" . $field . ">";
+            $fields_xml .= '
+				<'.$field.'>'.$value.'</'.$field.'>';
         }
-        $fields_xml .= "</fields>";
+        $fields_xml .= '</fields>';
+
         return $fields_xml;
     }
 
     /**
-     *    преобразование массива фильтров в xml
+     *    преобразование массива фильтров в xml.
+     *
      * @param $filters
+     *
      * @return string
      */
     private function _prepareFilters($filters)
     {
-        $filters_xml = "<filters>";
+        $filters_xml = '<filters>';
 
         foreach ($filters as $filter) {
-            $filters_xml .= "<filter>
-				<field>" . $filter["field"] . "</field>
-				<operation>" . $filter["operation"] . "</operation>
-				<value>" . $filter["value"] . "</value>
-				</filter>";
+            $filters_xml .= '<filter>
+				<field>'.$filter['field'].'</field>
+				<operation>'.$filter['operation'].'</operation>
+				<value>'.$filter['value'].'</value>
+				</filter>';
         }
 
-        $filters_xml .= "</filters>";
+        $filters_xml .= '</filters>';
+
         return $filters_xml;
     }
 
     /**
      * @param $orders
+     *
      * @return string
      */
     private function _prepareOrders($orders)
     {
-        $orders_xml = "<orders>";
+        $orders_xml = '<orders>';
 
         foreach ($orders as $order) {
-            $orders_xml .= "<order>
-				<field>" . $order["field"] . "</field>
-				<type>" . $order["type"] . "</type>
-				</order>";
+            $orders_xml .= '<order>
+				<field>'.$order['field'].'</field>
+				<type>'.$order['type'].'</type>
+				</order>';
         }
-        $orders_xml .= "</orders>";
+        $orders_xml .= '</orders>';
+
         return $orders_xml;
     }
 
     /**
      * @param $limits
+     *
      * @return string
      */
     private function _prepareLimits($limits)
     {
-        $filters_xml = "";
+        $filters_xml = '';
 
         if (count($limits)) {
-            $filters_xml .= "<limit>
-				<first>" . $limits[0] . "</first>
-				<number>" . $limits[1] . "</number>
-				</limit>";
+            $filters_xml .= '<limit>
+				<first>'.$limits[0].'</first>
+				<number>'.$limits[1].'</number>
+				</limit>';
         }
 
         return $filters_xml;
     }
 
     /**
-     *    создание запроса на добавление данных
+     *    создание запроса на добавление данных.
+     *
      * @param $structure
      * @param array $fields
+     *
      * @return SimpleXMLElement
      */
-    public function add($structure, $fields = array())
+    public function add($structure, $fields = [])
     {
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='add' uid='80085'>
-			<structure name='" . $structure . "'>" . "
-			" . $this->_prepareFields($fields) . "     								
+			<structure name='".$structure."'>".'
+			'.$this->_prepareFields($fields).'     								
 			</structure>
 			</action>
-			</request>";
+			</request>';
 
-
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 
     /**
      * @param $structure
      * @param $id
      * @param array $fields
+     *
      * @return SimpleXMLElement
      */
-    public function fileAdd($structure, $id, $fields = array())
+    public function fileAdd($structure, $id, $fields = [])
     {
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='fileAdd' uid='80085'>
-			<structure name='" . $structure . "' id='" . $id . "'>" . $this->_prepareFields($fields) . "</structure>
+			<structure name='".$structure."' id='".$id."'>".$this->_prepareFields($fields).'</structure>
 			</action>
-			</request>";
+			</request>';
 
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 
     /**
      * @param $id
+     *
      * @return SimpleXMLElement
      */
     public function paymentCalc($id)
@@ -272,34 +309,35 @@ class Xml_api
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='paymentCalc' uid='80085'>
-			<structure id='" . $id . "'></structure>
+			<structure id='".$id."'></structure>
 			</action>
 			</request>";
 
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 
     /**
-     *    создание запроса на редактирование данных
+     *    создание запроса на редактирование данных.
+     *
      * @param $structure
      * @param array $fields
      * @param array $filters
+     *
      * @return SimpleXMLElement
      */
-    public function update($structure, $fields = array(), $filters = array())
+    public function update($structure, $fields = [], $filters = [])
     {
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='edit' uid='80085'>
-			<structure name='" . $structure . "'>" . "
-			" . $this->_prepareFields($fields) . "
-			" . $this->_prepareFilters($filters) . "     								
+			<structure name='".$structure."'>".'
+			'.$this->_prepareFields($fields).'
+			'.$this->_prepareFilters($filters).'     								
 			</structure>
 			</action>
-			</request>";
+			</request>';
 
-
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 
     /**
@@ -307,6 +345,7 @@ class Xml_api
      *
      * @param $structure
      * @param $id
+     *
      * @return SimpleXMLElement
      */
     public function delete($structure, $id)
@@ -314,11 +353,11 @@ class Xml_api
         $request = "<?xml version='1.0' encoding='utf8' ?>
 			<request>
 			<action type='wipe' uid='80085'>
-			<structure name='" . $structure . "' id='" . $id . "'>     								
+			<structure name='".$structure."' id='".$id."'>     								
 			</structure>
 			</action>
 			</request>";
 
-        return $this->_parse($this->send($this->url, "POST", array("xml" => $request), array("sess_id" => $this->sess_id)));
+        return $this->_parse($this->send($this->url, 'POST', ['xml' => $request], ['sess_id' => $this->sess_id]));
     }
 }
